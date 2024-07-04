3a0824c9e3ebacc9cf86d42f3243a76d96bfe34f
Sergio D. Plaza <sergiodamianplaza@gmail.com>
2024-07-04 15:22:03 -0300

    Add files via upload

---
+++ TP parte 2 - FS PYTHON - Com. 24159 - Grupo 14/script sql grupo 14.sql
@@ -0,0 +1,18 @@
+create database grupo14_db;
+use grupo14_db;
+
+create table form_contacto(
+	Id int(4) primary key auto_increment,
+    Fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    Motivo ENUM('Publicar un aviso', 'Opinar sobre un servicio', 'Hacer una consulta'),
+    Nombre VARCHAR(20),
+    Apellido VARCHAR(20),
+    E_mail VARCHAR (30),
+    Whatsapp INT(15),
+    Mensaje VARCHAR(500),
+    Adjunto ENUM('SI', 'NO'),
+    TC ENUM('SI', 'NO'),
+    Observaciones VARCHAR(100)
+);
+
+select * from form_contacto;
